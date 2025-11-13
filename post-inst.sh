d273c72fe6a4fab69ec0daf320858e8db232e5c2
Essias Souza <doutorska@gmail.com>
2025-11-13 18:06:35 -0300

    Update post-inst.sh

--- post-inst.sh
+++ post-inst.sh
@@ -1,4 +1,4 @@
-#/bin/bash
+#!/bin/bash
 
 sudo apt update
 sudo apt install apache2 php libapache2-mod-php mysql-server php-mysql -y
@@ -11,5 +11,5 @@ sudo ufw status
 sudo apt install ufw -y
 sudo ufw allow 22/tcp
 sudo ufw allow 80/tcp
-sudo ufw enable
+sudo ufw enable -y
 sudo reboot
